6bd04a2d8a5e45020dd2baebfe00c6ff366f93d5
doanvanquoc <vanquoc.dev@gmail.com>
2024-03-21 10:28:46 +0700

    fix socket

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
     socket.on('login', (data) => {
-        userController.updateSocketId(data.id, socket.id);
-        console.log('login', data.id, socket.id);
+        userController.updateSocketId(data, socket.id);
+        console.log('login', data, socket.id);
     });
     socket.on('send_message', (data) => {
         io.emit('sendMessage', data);
